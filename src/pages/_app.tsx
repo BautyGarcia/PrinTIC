12f7d8f89b3207929828d0090051d9c855f7b21b
Bauty Garcia <bautistaroccogarcia@gmail.com>
2023-11-16 21:16:14 -0300

    setup mantine and removed template stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-
+import { MantineProvider, createTheme } from '@mantine/core';
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+const theme = createTheme({
+  fontFamily: 'Comic sans ms, Open Sans, sans-serif',
+  primaryColor: 'purple',
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <MantineProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
   );
 };
 
