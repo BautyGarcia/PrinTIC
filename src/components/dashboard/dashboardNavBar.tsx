9469cea407cd54c8dfc890908475063734288534
Bauty Garcia <bautistaroccogarcia@gmail.com>
2023-11-18 22:20:14 -0300

    fix(dashboard): fixed rendering issue with classnames

--- src/components/dashboard/dashboardNavBar.tsx
+++ src/components/dashboard/dashboardNavBar.tsx
@@ -1,13 +1,23 @@
 import { useWindowSize } from "~/hooks/useWindowSize";
 import UserBox from "./userBox";
+import { useEffect, useState } from "react";
 
 const DashboardNavBar = ({ isOpened, nombre, curso }: { isOpened: boolean, nombre: string, curso: string }) => {
+    const [isMounted, setIsMounted] = useState(false);
     const { width } = useWindowSize();
     const isMobile = width < 800;
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
     
     return (
         <section className={isMobile ? 
-            `dashboard-nav ${isOpened ? "dashboard-nav-open" : ""} md:block h-full bg-appshell_background` :
+            `${isOpened ? "dashboard-nav-open" : "dashboard-nav"} md:block h-full bg-appshell_background` :
             `w-[300px] h-full bg-appshell_background`
         }>
             {/* Este div se muestra solo en Desktop mode */}
