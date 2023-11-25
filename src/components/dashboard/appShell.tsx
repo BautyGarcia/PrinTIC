e53ba30206261b4fea0f15d462e146da28b96542
Bauty Garcia <bautistaroccogarcia@gmail.com>
2023-11-25 14:05:38 -0300

    fix: responsive issues on solicitudes page

--- src/components/dashboard/appShell.tsx
+++ src/components/dashboard/appShell.tsx
@@ -1,13 +1,16 @@
 import DashboardNavBar from "./dashboardNavBar";
 import DashboardHeader from "./dashboardHeader";
 import React, { useState } from "react";
+import { useWindowSize } from "~/hooks/useWindowSize";
 
 const AppShell = ({ children, nombre, curso }: { children: React.ReactNode, nombre: string, curso: string }) => {
   const [opened, setOpened] = useState(false);
+  const { width } = useWindowSize();
+  const isOverflowHidden = width > 1120;
 
   return (
     <>
-      <section className="relative overflow-hidden z-10 flex h-screen flex-col">
+      <section className={`relative ${isOverflowHidden ? "overflow-hidden" : ""} z-10 flex h-screen flex-col`}>
         <DashboardHeader opened={opened} setIsOpened={setOpened} nombre={nombre} curso={curso} />
         <div className="flex h-full">
           <DashboardNavBar isOpened={opened} nombre={nombre} curso={curso} setOpen={setOpened}/>
