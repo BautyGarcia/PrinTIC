f74de0f69e4bcb19167d602df8efdf88fd21fb6e
Bauty Garcia <bautistaroccogarcia@gmail.com>
2023-12-03 12:06:33 -0300

    feat(cuantoFalta): added loading state

--- src/pages/dashboard/cuantoFalta.tsx
+++ src/pages/dashboard/cuantoFalta.tsx
@@ -3,9 +3,10 @@ import Dashboard from ".";
 import Head from "next/head";
 import { api } from "~/utils/api";
 import { formatDate } from "~/utils/scripts";
+import { PageLoader } from "~/components/utils/loaders";
 
 const CuantoFalta: NextPage = () => {
-    const { data: pedidos } = api.pedidos.getAllPedidos.useQuery();
+    const { data: pedidos, isLoading } = api.pedidos.getAllPedidos.useQuery();
 
     return (
         <>
@@ -16,38 +17,45 @@ const CuantoFalta: NextPage = () => {
             </Head>
             <Dashboard isOverflowHidden={false}>
                 <div className="p-5 pb-28">
-                    <table className="w-full h-full bg-appshell_background solicitud">
-                        <thead className="">
-                            <tr className="h-[85px] font-spacemono text-[20px] bg-appshell_secondary text-left">
-                                <th className="px-4 w-[150px] ">Fecha</th>
-                                <th className="px-4">Alumno</th>
-                                <th className="px-4">Curso</th>
-                                <th className="px-4">Estado</th>
-                                <th className="px-4">Materia</th>
-                                <th className="px-4">Notas</th>
-                                <th className="px-4">Profesor</th>
-                                <th className="px-4 pr-5">Motivos</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                pedidos?.map((pedido, index) => {
-                                    return (
-                                        <tr key={index} className="h-[50px] text-lg border-b-2 border-b-appshell_secondary hover:bg-pink_tic_light">
-                                            <td className="px-4 py-4">{formatDate(pedido.fecha)}</td>
-                                            <td className="px-4 whitespace-nowrap">{pedido.user.name}</td>
-                                            <td className="px-4">{pedido.user.curso}</td>
-                                            <td className="px-4">{pedido.estado}</td>
-                                            <td className="px-4">{pedido.materia}</td>
-                                            <td className="px-4">{pedido.observacionesAlumno}</td>
-                                            <td className="px-4">{pedido.aprobador?.name ?? "Ninguno"}</td>
-                                            <td className="px-4">{pedido.observacionesProfesor}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+                    {
+                        isLoading ?
+                            <div className="flex justify-center items-center noHeaderScreen">
+                                <PageLoader />
+                            </div>
+                            :
+                            <table className="w-full h-full bg-appshell_background solicitud">
+                                <thead className="">
+                                    <tr className="h-[85px] font-spacemono text-[20px] bg-appshell_secondary text-left">
+                                        <th className="px-4 w-[150px] ">Fecha</th>
+                                        <th className="px-4">Alumno</th>
+                                        <th className="px-4">Curso</th>
+                                        <th className="px-4">Estado</th>
+                                        <th className="px-4">Materia</th>
+                                        <th className="px-4">Notas</th>
+                                        <th className="px-4">Profesor</th>
+                                        <th className="px-4 pr-5">Motivos</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        pedidos?.map((pedido, index) => {
+                                            return (
+                                                <tr key={index} className="h-[50px] text-lg border-b-2 border-b-appshell_secondary hover:bg-pink_tic_light">
+                                                    <td className="px-4 py-4">{formatDate(pedido.fecha)}</td>
+                                                    <td className="px-4 whitespace-nowrap">{pedido.user.name}</td>
+                                                    <td className="px-4">{pedido.user.curso}</td>
+                                                    <td className="px-4">{pedido.estado}</td>
+                                                    <td className="px-4">{pedido.materia}</td>
+                                                    <td className="px-4">{pedido.observacionesAlumno}</td>
+                                                    <td className="px-4">{pedido.aprobador?.name ?? "Ninguno"}</td>
+                                                    <td className="px-4">{pedido.observacionesProfesor}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                    }
                 </div>
             </Dashboard>
         </>
