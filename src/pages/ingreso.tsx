67ca1ade7e6aa4f6a8036870c3aaa66cd63264f0
Bauty Garcia <bautistaroccogarcia@gmail.com>
2024-01-14 20:47:15 +0100

    Changed ingreso error handling to toasts

--- src/pages/ingreso.tsx
+++ src/pages/ingreso.tsx
@@ -8,12 +8,13 @@ import { Title } from "~/components/utils/texts";
 import { ActionButton } from "~/components/utils/buttons";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import { toast } from "react-toastify";
 
 const Ingreso: NextPage = () => {
     const [mail, setMail] = useState("")
     const [password, setPassword] = useState("")
     const [loading, setLoading] = useState(false)
-    const [errorMessage, setErrorMessage] = useState("");
+    const [isError, setIsError] = useState(false);
     const router = useRouter();
     const { mutate: checkPasswordChange } = api.users.isPasswordChanged.useMutation();
     let isPasswordChanged = false;
@@ -22,7 +23,8 @@ const Ingreso: NextPage = () => {
         e.preventDefault();
 
         if (!(mail.endsWith("@est.ort.edu.ar") || mail.endsWith("@ort.edu.ar"))) {
-            setErrorMessage("El mail debe ser de ORT");
+            toast.error("El mail debe ser de ORT");
+            setIsError(true);
             return;
         }
         setLoading(true);
@@ -34,7 +36,8 @@ const Ingreso: NextPage = () => {
                 isPasswordChanged = data;
             },
             onError: (error) => {
-                setErrorMessage(error.message ?? "Hubo un error al verificar si la contraseña fue cambiada");
+                toast.error(error.message ?? "Hubo un error al verificar si la contraseña fue cambiada");
+                setIsError(true);
             }
         });
 
@@ -45,7 +48,7 @@ const Ingreso: NextPage = () => {
         }).then((response) => {
             if (response?.ok) {
                 setLoading(false);
-                setErrorMessage("");
+                setIsError(false);
                 if (isPasswordChanged) {
                     void router.push("/dashboard");
                     return;
@@ -53,7 +56,8 @@ const Ingreso: NextPage = () => {
                 void router.push("/nuevaClave");
             } else {
                 setLoading(false);
-                setErrorMessage(response?.error ?? "Email o contraseña incorrectos");
+                setIsError(true);
+                toast.error(response?.error ?? "Email o contraseña incorrectos");
             }
         });
     }
@@ -69,9 +73,8 @@ const Ingreso: NextPage = () => {
                 <Title>{"<Ingreso/>"}</Title>
                 <div className="flex flex-col w-full min-h-2/6 md:w-2/3 lg:w-1/3 bg-container_background rounded-lg p-5">
                     <form className="flex flex-col w-full gap-6" onSubmit={(e) => handleSubmit(e)}>
-                        <TextInput titleClassName="text-lg font-roboto" label="Mail" placeholder="DNI@est.ort.edu.ar" value={mail} setValue={setMail} isError={!(errorMessage.length === 0)} />
-                        <PasswordInput className="w-full" label="Contraseña" placeholder="inventaronelVAR" value={password} setValue={setPassword} isError={!(errorMessage.length === 0)} />
-                        {errorMessage.length > 0 && <p className="text-red_tic text-center text-lg">{errorMessage}</p>}
+                        <TextInput titleClassName="text-lg font-roboto" label="Mail" placeholder="DNI@est.ort.edu.ar" value={mail} setValue={setMail} isError={isError} />
+                        <PasswordInput className="w-full" label="Contraseña" placeholder="inventaronelVAR" value={password} setValue={setPassword} isError={isError} />
 
                         <div className="flex flex-col gap-1 mt-5">
                             <ActionButton
