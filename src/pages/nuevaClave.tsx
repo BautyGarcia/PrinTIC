de309b4ecd501bddbc2624ea3df7e94d0e3ea18b
Bauty Garcia <bautistaroccogarcia@gmail.com>
2024-01-14 20:49:33 +0100

    added head tag to nuevaClave page

--- src/pages/nuevaClave.tsx
+++ src/pages/nuevaClave.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { toast } from "react-toastify";
@@ -34,29 +35,36 @@ const NuevaClave: NextPage = () => {
   }
 
   return (
-    <div className="relative z-10 flex items-center justify-center h-screen w-screen">
-      <div className="bg-purple_tic p-6 lg:p-12 rounded-lg flex flex-col gap-12 mx-4 max-w-[500px]">
-        <div className="flex flex-col">
-          <Title className="text-center text-[30px] sm:text-[35px] leading-none">{`¡Hola ${sessionData?.user?.name}!`}</Title>
-          <Subtitle className="text-[15px] sm:text-[20px] text-center">Escribí tu nueva contraseña</Subtitle>
-        </div>
-        <div className="flex flex-col gap-4">
-          <PasswordInput
-            isError={false}
-            label=""
-            className="w-full"
-            placeholder="********"
-            value={newPassword}
-            setValue={setNewPassword}
-          />
-          <ActionButton
-            isLoading={isChangingPassword}
-            onClick={handlePasswordChange}
-            withAnimation
-          >Cambiar</ActionButton>
+    <>
+      <Head>
+        <title>PrinTIC - Nueva Contraseña</title>
+        <meta name='description' content='PrinTIC' />
+        <link rel='icon' href='/general/ticLogo.ico' />
+      </Head>
+      <div className="relative z-10 flex items-center justify-center h-screen w-screen">
+        <div className="bg-purple_tic p-6 lg:p-12 rounded-lg flex flex-col gap-12 mx-4 max-w-[500px]">
+          <div className="flex flex-col">
+            <Title className="text-center text-[30px] sm:text-[35px] leading-none">{`¡Hola ${sessionData?.user?.name}!`}</Title>
+            <Subtitle className="text-[15px] sm:text-[20px] text-center">Escribí tu nueva contraseña</Subtitle>
+          </div>
+          <div className="flex flex-col gap-4">
+            <PasswordInput
+              isError={false}
+              label=""
+              className="w-full"
+              placeholder="********"
+              value={newPassword}
+              setValue={setNewPassword}
+            />
+            <ActionButton
+              isLoading={isChangingPassword}
+              onClick={handlePasswordChange}
+              withAnimation
+            >Cambiar</ActionButton>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
